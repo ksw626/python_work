b058ee84b01e43a7a944a9c2aa590136ae3720b3
ksw626 <34931154+ksw626@users.noreply.github.com>
2018-02-18 16:44:12 +0900

    Delete ex1_2.py

--- ex1_2.py
+++
@@ -1,9 +0,0 @@
-#입력하여 해당 단수를 출력
-
-number = input('number: ')
-
-print('-------' + number + 'DAN' + '-------')
-    
-for value in range (1,10):
-    result = int(number) * value
-    print(str(number) + ' * ' + str(value) + ' = ' + str(result))

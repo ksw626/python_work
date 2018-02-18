61e9b4a6ee54a56f3ca85ae15892c5b8231a322a
ksw626 <34931154+ksw626@users.noreply.github.com>
2018-02-18 16:22:42 +0900

    Add files via upload

---
+++ ex1_1.py
@@ -0,0 +1,12 @@
+#구구단을 1단부터 9단까지 출력
+
+for dan in range (1,10):
+    print('-------' + str(dan) + 'DAN' + '-------')
+
+    for value in range (1,10):
+        if dan % 2 == 1:
+            result = dan * value
+            print(str(dan) + ' * ' + str(value) + ' = ' + str(result))
+        else:
+            print('SKIP')
+            break
